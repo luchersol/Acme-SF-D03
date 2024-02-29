f51eeef0f74ff2941fb9bff91b413af0a164af33
José Manuel Miret Martín <josmirmar2@alum.us.es>
2024-02-29 13:22:16 +0100

    Task6-G, pull request modificaciones

--- src/main/java/acme/entities/notice/Notice.java
+++ src/main/java/acme/entities/notice/Notice.java
@@ -8,24 +8,27 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.client.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Notice {
+public class Notice extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
 	@Past
 	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
 	private Date				instantiationMoment;
 
 	@NotBlank
