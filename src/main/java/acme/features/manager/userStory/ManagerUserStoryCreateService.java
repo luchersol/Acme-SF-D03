b52425da3247c9a5eefe05540bdabeb7fc7470d0
luchersol <lmherencia2003@gmail.com>
2024-03-08 20:54:30 +0100

    Creacion de plantilla de repositorio, servicios y controlador de historias de usuario

---
+++ src/main/java/acme/features/manager/userStory/ManagerUserStoryCreateService.java
@@ -0,0 +1,85 @@
+/*
+ * AdministratorDashboardShowService.java
+ *
+ * Copyright (C) 2012-2024 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.manager.userStory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.project.Project;
+import acme.roles.Manager;
+
+@Service
+public class ManagerUserStoryCreateService extends AbstractService<Manager, Project> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ManagerUserStoryRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Project object = new Project();
+		object.setCode("");
+		object.setTitle("");
+		object.setAbstractProject("");
+		object.setIndication(false);
+		object.setCost(null);
+		object.setLink(null);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void bind(final Project object) {
+		Dataset dataset;
+
+		dataset = super.unbind(object, "");
+
+		super.getResponse().addData(dataset);
+	}
+
+	@Override
+	public void validate(final Project object) {
+		assert object != null;
+
+		boolean confirmation;
+
+		confirmation = super.getRequest().getData("confirmation", boolean.class);
+		super.state(confirmation, "confirmation", "javax.validation.constraints.AssertTrue.message");
+	}
+
+	@Override
+	public void perform(final Project object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Project object) {
+		assert object != null;
+
+		//		super.getResponse().addData(dataset);
+	}
+
+}
