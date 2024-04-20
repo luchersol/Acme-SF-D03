59ad65e7fe8c03c63c1a925229855fef79c8a080
luchersol <lmherencia2003@gmail.com>
2024-04-20 13:26:13 +0200

    Eliminar cosas de la relación entre project y risk

--- src/main/java/acme/entities/risk/Risk.java
+++ src/main/java/acme/entities/risk/Risk.java
@@ -5,9 +5,11 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -60,9 +62,10 @@ public class Risk extends AbstractEntity {
 		return this.probability * this.impact;
 	}
 
+
 	@NotNull
 	@Valid
 	@ManyToOne(optional = false)
-	private Administrator	administrator;
+	private Administrator administrator;
 
 }
