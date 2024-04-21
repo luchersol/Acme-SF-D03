51a25badbf7898a59e4cd7b7a366ed12c613ca1f
José Manuel Miret Martín <josmirmar2@alum.us.es>
2024-04-21 13:07:42 +0200

    Task 18 student 4, Edit and update the profile of a sponsor

--- src/main/java/acme/features/authenticated/sponsor/AuthenticatedSponsorController.java
+++ src/main/java/acme/features/authenticated/sponsor/AuthenticatedSponsorController.java
@@ -16,7 +16,10 @@ public class AuthenticatedSponsorController extends AbstractController<Authentic
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	private AuthenticatedSponsorCreateService createService;
+	private AuthenticatedSponsorCreateService	createService;
+
+	@Autowired
+	private AuthenticatedSponsorUpdateService	updateService;
 
 	// Constructors -----------------------------------------------------------
 
@@ -24,6 +27,8 @@ public class AuthenticatedSponsorController extends AbstractController<Authentic
 	@PostConstruct
 	protected void initialise() {
 		super.addBasicCommand("create", this.createService);
+		super.addBasicCommand("update", this.updateService);
+
 	}
 
 }
