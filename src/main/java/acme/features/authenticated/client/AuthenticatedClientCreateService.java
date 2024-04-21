f5219c7be320d1d934b38d8784cf189d0f230ec6
Marina Calero <73230580+marcallop7@users.noreply.github.com>
2024-04-21 19:55:51 +0200

    Update AuthenticatedClientCreateService.java

--- src/main/java/acme/features/authenticated/client/AuthenticatedClientCreateService.java
+++ src/main/java/acme/features/authenticated/client/AuthenticatedClientCreateService.java
@@ -80,7 +80,6 @@ public class AuthenticatedClientCreateService extends AbstractService<Authentica
 		choicesType = SelectChoices.from(TypeClients.class, client.getType());
 
 		dataset = super.unbind(client, "identification", "companyName", "type", "email", "link");
-		dataset.put("type", choicesType.getSelected().getClass());
 		dataset.put("types", choicesType);
 		super.getResponse().addData(dataset);
 	}
